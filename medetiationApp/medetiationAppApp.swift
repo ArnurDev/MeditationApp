d3ce671374039357c4adafc1c245fb23d658682d
ArnurDev <mamyrovarnur99@gmail.com>
2025-12-05 08:11:00 +0500

    Initial Commit

---
+++ medetiationApp/medetiationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  medetiationAppApp.swift
+//  medetiationApp
+//
+//  Created by Гаухар Байкен on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct medetiationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
